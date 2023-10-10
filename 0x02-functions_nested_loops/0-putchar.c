1f9dd4f435ee2ae1bc4a582fdb245fffdc3c2b56
dawit <dawitxxd@gmail.com>
2023-10-10 02:37:56 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,16 +8,16 @@
  */
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int len = sizeof(str) / sizeof)str[0];
-	int nl = 10;
+	char str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int len = sizeof(str) / sizeof(str[0]);
+	char nl = 10;
 	int i = 0;
 
 	while (i < len)
 	{
-		_putchar(str[i]);
+		_putchar(&str[i]);
 		i++;
 	}
-	_putchar(nl);
+	_putchar(&nl);
 	return (0);
 }
