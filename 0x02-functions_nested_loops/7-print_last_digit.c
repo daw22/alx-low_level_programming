70c9c1d1e5df6f7abe3ebd43ee3c326faf85d5b4
dawit <dawitxxd@gmail.com>
2023-10-10 08:09:07 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,16 @@
 #include "main.h"
 /**
  * print_last_digit- prints the last digit
- * @n: numbet input
+ * @n: number input
  *
  * Return: the last digit of input number
  */
 int print_last_digit(int n)
 {
-	return ((n < 10 && n >= 0) ? n : (n > -10 && n < 0) ? -(n) : n > 10 ? n % 10 : -(n % 10));
+	int ld;
+	
+	n = n < 0 ? -(n) : n;
+	ld = n < 10 ? n : n % 10;
+	_putchar('0' + ld);
+	return (ld);
 }
