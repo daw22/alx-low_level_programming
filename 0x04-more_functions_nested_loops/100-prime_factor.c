8d6969938b2efa717ea04e436c1b4a234c976b88
dawit <dawitxxd@gmail.com>
2023-10-12 12:54:42 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	long n = 1231952;
+	long largest_prime_factor = -1;
+	int i = 2;
+
+	while (i * i <= n)
+	{
+		while (n % i == 0)
+		{
+			largest_prime_factor = i;
+			n /= i;
+		}
+		i += 1;
+	}
+	if (n > 1)
+		largest_prime_factor = n;
+	printf("%lu\n", largest_prime_factor);
+	return (0);
+}
