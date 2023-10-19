35f509ba6ce3a56a54ad9c5e823fd5ef53f42018
dawit <dawitxxd@gmail.com>
2023-10-19 05:49:05 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rot13 - incrypts input string with rot-13
+ * @str: input string
+ *
+ * Return: pointer to encrypted string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char *al = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *tr = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == al[j])
+				str[i] = tr[j];
+		}
+	}
+	return (str);
+}
+		
+
+	
