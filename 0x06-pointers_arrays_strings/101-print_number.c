3395b5d0022c7c6df54aaf2617c4f6baf9e7820e
dawit <dawitxxd@gmail.com>
2023-10-19 07:42:14 -0700

    Numbers have life; they're not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+int powten(int n);
+/**
+ * print_number - prints numbers
+ * @n: input number(int)
+ */
+void print_number(int n)
+{
+	int i, tmp = n, len = 1;
+	
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (tmp / 10)
+	{
+		len++;
+		tmp /= 10;
+	}
+
+	for (i = len; i > 0; i--)
+	{
+		_putchar((n / powten(i - 1)) + '0');
+		n -= (n / powten(i - 1)) * powten(i - 1);
+	}
+}
+
+/**
+ * powten - ten raised to input
+ * @n: input int
+ * Return: int
+ */
+int powten(int n)
+{
+	int i, res = 1;
+
+	for (i = 0; i < n; i++)
+	{
+		res *= 10;
+	}
+	return (res);
+}
