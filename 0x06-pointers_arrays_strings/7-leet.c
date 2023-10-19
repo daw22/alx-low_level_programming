0278fc5ea41dd15adeffea870f0f9751f51afcfa
dawit <dawitxxd@gmail.com>
2023-10-19 00:44:23 -0700

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <string.h>
 
-int is_in_array(char, char*, int);
 /**
  * leet - encodes string with leet code
  * @str: input string
@@ -10,34 +9,18 @@ int is_in_array(char, char*, int);
  */
 char *leet(char *str)
 {
-	int i, index, len = strlen(str);
+	int i, j, len = strlen(str);
 	char dchar[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int echar[] = {4, 3, 0, 7, 1};
+	int dclen = sizeof(dchar) / sizeof(char);
 
 	for (i = 0; i < len; i++)
 	{
-		index = is_in_array(str[i], dchar, 10);
-		if (index >= 0)
-			str[i] = echar[index / 2] + '0';
+		for (j = 0; j < dclen; j++)
+		{
+			if (dchar[j] == str[i])
+				str[i] = echar[j / 2] + '0';
+		}
 	}
 	return (str);
-}
-
-/**
- * is_in_array - checks if input is in the given array
- * @c: input character
- * @arr: array to search in
- * @n: arra coutnt
- * Return: int index if found, -1 if not found
- */
-int is_in_array(char c, char *arr, int n)
-{
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		if (arr[i] == c)
-			return (i);
-	}
-	return (-1);
 }
