1791f353be14ccc0ae82b98e2e764c7cbec292ee
dawit <dawitxxd@gmail.com>
2023-10-25 06:50:22 -0700

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strangeDreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of a string
+ * @str: input string
+ * Return: int 
+ */
+int _strlen_recursion(char *str)
+{
+	if(*str == '\0')
+	{
+		return 0;
+	}
+	str++;
+	return 1 + _strlen_recursion(str);
+}
