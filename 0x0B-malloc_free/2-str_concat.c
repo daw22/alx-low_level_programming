27fdf7cdbfd73217b0104083900b23f00ef7c9e7
dawit <dawitxxd@gmail.com>
2023-10-30 00:44:01 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-str_concat.c -o c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatinates strings
+ * @str1: first string input
+ * @str2: second string input
+ * Return: pointer to concatinated string
+ */
+char *str_concat(char *str1, char *str2)
+{
+	unsigned int i, len, len1, len2;
+	char *p;
+
+	if (str1 == NULL)
+		str1 = "";
+	if (str2 == NULL)
+		str2 = "";
+	len1 = strlen(str1);
+	len2 = strlen(str2);
+	len = len1 + len2;
+	p = malloc(len + 1);
+	for (i = 0; i < len1; i++)
+		p[i] = str1[i];
+	for (i = len1; i < len; i++)
+		p[i] = str2[i - len1];
+	p[len] = '\0';
+	return (p);
+}
