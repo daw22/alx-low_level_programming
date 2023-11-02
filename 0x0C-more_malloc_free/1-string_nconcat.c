87f84ec1f60a2260af8f645dc0fac3ad05056e9e
dawit <dawitxxd@gmail.com>
2023-11-02 04:03:30 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n > strlen(s2))
+	if (n >= strlen(s2))
 		n = strlen(s2);
 	p = malloc(strlen(s1) + strlen(s2) + 1);
 	if (p == NULL)
