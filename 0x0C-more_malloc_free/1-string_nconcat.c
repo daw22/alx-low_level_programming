749d077ed1fb8a4b5bfe3efe85a5b44335d2922e
dawit <dawitxxd@gmail.com>
2023-11-02 04:38:18 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,22 +9,24 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i, lens1, lens2;
 	char *p;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n >= strlen(s2))
+	for (lens1 = 0; s1[lens1] != '\0'; lens1++);
+	for (lens2 = 0; s2[lens2] != '\0'; lens2++);
+	if (n >= lens2)
 		n = strlen(s2);
-	p = malloc(strlen(s1) + strlen(s2) + 1);
+	p = malloc(lens1 + lens2 + 1);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < strlen(s1); i++)
+	for (i = 0; i < lens1; i++)
 		p[i] = s1[i];
-	for (i = strlen(s1); i < (strlen(s1) + n); i++)
-		p[i] = s2[i - strlen(s1)];
+	for (i = lens1; i < (lens1 + n); i++)
+		p[i] = s2[i - lens1];
 	p[i] = '\0';
 	return (p);
 }
