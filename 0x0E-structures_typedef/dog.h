91a015c081ea2051843c9f56675ce365cd361cee
dawit <dawitxxd@gmail.com>
2023-11-06 01:49:10 -0800

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct for dog type
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: defines dog type with name, age and owner properties
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
