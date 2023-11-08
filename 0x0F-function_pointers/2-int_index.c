952af8cb79d671cf24d451c32e3739936f51f0e6
dawit <dawitxxd@gmail.com>
2023-11-07 22:54:22 -0800

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - finds int in an integer array
+ * @arr: input array
+ * @size: size of the array
+ * @cmp: callback function the iplements the search
+ *
+ * Return: index of first match, -1 if no match found
+ */
+int int_index(int *arr, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		int r = cmp(arr[i]);
+
+		if (r != 0)
+			return (i);
+	}
+	return (-1);
+}
