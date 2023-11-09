290906f8d21220ee23274e315ec395a43698deaa
dawit <dawitxxd@gmail.com>
2023-11-09 09:02:11 -0800

    To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_all - prints all types of args
+ * @format: format string
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *str;
+	int ignore = 0;
+
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char*);
+				if (str)
+					printf("%s", str);
+				else
+					printf("(nil)");
+				break;
+			default:
+				ignore = 1;
+				break;
+		}
+		if (!ignore && i < (strlen(format) - 1))
+			printf(", ");
+		ignore = 0;
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,6 @@ void _putchar(char);
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char*, const unsigned int, ...);
 void print_strings(const char*, const unsigned int, ...);
+void print_all(const char* const format, ...);
 
 #endif
