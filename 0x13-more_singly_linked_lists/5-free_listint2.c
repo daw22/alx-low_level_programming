427333dfe53e52f0e49a6946e4e199808f5a45bd
dawit <dawitxxd@gmail.com>
2023-11-20 00:58:14 -0800

    Free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a linked list and set haad to NULL
+ * @head: pointer to a pointer to head
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *h;
+
+	while (*head)
+	{
+		h = (*head)->next;
+		free(*head);
+		*head = h;
+	}
+}
