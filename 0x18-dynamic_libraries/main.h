20211ade37e76efe55b5a4519e88368f0b03e91c
Dawit Yifru <dawitxxd@gmail.com>
2024-01-15 12:15:36 +0300

    heades

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,4 +22,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+/** operations **/
+int add(int, int);
+int sub(int, int);
+int div(int, int);
+int mul(int, int);
+int mod(int, int);
 #endif
